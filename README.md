9f03cdd47683bea36f7a1eea3d6b122a74baf091
Sion Pixley <sionpixley@gmail.com>
2024-11-10 22:12:44 -0600

    Doing random strings only now

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Delirium
 
-Delirium is a Go library that produces cryptographically-secure random values. Delirium is a wrapper around the standard library [crypto/rand](https://pkg.go.dev/crypto/rand), but it makes it easier to produce values of smaller size than crypto/rand is usually used for.
+Delirium is a Go library that produces cryptographically-secure random string values. Delirium is a wrapper around the standard library [crypto/rand](https://pkg.go.dev/crypto/rand).
 
 ## How to install
 
@@ -12,34 +12,18 @@ Make sure to include the import:
 
 `import "github.com/sionpixley/delirium/pkg/delirium"`
 
-### Producing random bytes
+### Making random bytes
 
-`randomBytes := delirium.RandomBytes(<insert-num-of-bytes>)`
+`randomBytes := delirium.MakeRandomBytes(<insert-num-of-bytes>)`
 
-### Producing random base64 strings
+### Making random base64 strings
 
-`randomBase64 := delirium.RandomBase64String(<insert-num-of-bytes>)`
+`randomBase64 := delirium.MakeRandomBase64String(<insert-num-of-bytes>)`
 
-### Producing random integers
+### Making random URL-safe base64 strings
 
-Delirium can produce random signed and unsigned integers of sizes: 16-bit, 32-bit, and 64-bit.
+`randomBase64 := delirium.MakeRandomBase64UrlString(<insert-num-of-bytes>)`
 
-```
-randomInt16 := delirium.RandomInt16()
-randomUint16 := delirium.RandomUint16()
+### Making random hexadecimal strings
 
-randomInt32 := delirium.RandomInt32()
-randomUint32 := delirium.RandomUint32()
-
-randomInt64 := delirium.RandomInt64()
-randomUint64 := delirium.RandomUint64()
-```
-
-### Producing random floats
-
-Delirium can produce random float32 or float64 values.
-
-```
-randomFloat32 := delirium.RandomFloat32()
-randomFloat64 := delirium.RandomFloat64()
-```
+`randomHex := delirium.MakeRandomHexString(<insert-num-of-bytes>)`
--- pkg/delirium/delirium.go
+++ pkg/delirium/delirium.go
@@ -3,13 +3,12 @@ package delirium
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/binary"
-	"math"
+	"encoding/hex"
 )
 
 // Makes a cryptographically-secure random base64 string.
-func RandomBase64String(numOfBytes int) (string, error) {
-	b, err := RandomBytes(numOfBytes)
+func MakeRandomBase64String(numOfBytes int) (string, error) {
+	b, err := MakeRandomBytes(numOfBytes)
 	if err != nil {
 		return "", err
 	}
@@ -17,89 +16,29 @@ func RandomBase64String(numOfBytes int) (string, error) {
 	return base64.StdEncoding.EncodeToString(b), nil
 }
 
+// Makes a cryptographically-secure random base64 URL-safe string.
+func MakeRandomBase64UrlString(numOfBytes int) (string, error) {
+	b, err := MakeRandomBytes(numOfBytes)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
 // Makes a cryptographically-secure random []byte.
-func RandomBytes(numOfBytes int) ([]byte, error) {
+func MakeRandomBytes(numOfBytes int) ([]byte, error) {
 	b := make([]byte, numOfBytes)
 	_, err := rand.Read(b)
 	return b, err
 }
 
-// Makes a cryptographically-secure random int16.
-func RandomInt16() (int16, error) {
-	b, err := RandomBytes(2)
+// Makes a cryptographically-secure random hexadecimal string.
+func MakeRandomHexString(numOfBytes int) (string, error) {
+	b, err := MakeRandomBytes(numOfBytes)
 	if err != nil {
-		return 0, nil
+		return "", err
 	}
 
-	return int16(binary.NativeEndian.Uint16(b)), nil
-}
-
-// Makes a cryptographically-secure random int32.
-func RandomInt32() (int32, error) {
-	b, err := RandomBytes(4)
-	if err != nil {
-		return 0, err
-	}
-
-	return int32(binary.NativeEndian.Uint32(b)), nil
-}
-
-// Makes a cryptographically-secure random int64.
-func RandomInt64() (int64, error) {
-	b, err := RandomBytes(8)
-	if err != nil {
-		return 0, nil
-	}
-
-	return int64(binary.NativeEndian.Uint64(b)), nil
-}
-
-// Makes a cryptographically-secure random float32.
-func RandomFloat32() (float32, error) {
-	b, err := RandomBytes(4)
-	if err != nil {
-		return 0, err
-	}
-
-	return math.Float32frombits(binary.NativeEndian.Uint32(b)), nil
-}
-
-// Makes a cryptographically-secure random float64.
-func RandomFloat64() (float64, error) {
-	b, err := RandomBytes(8)
-	if err != nil {
-		return 0, nil
-	}
-
-	return math.Float64frombits(binary.NativeEndian.Uint64(b)), nil
-}
-
-// Makes a cryptographically-secure random uint16.
-func RandomUint16() (uint16, error) {
-	b, err := RandomBytes(2)
-	if err != nil {
-		return 0, nil
-	}
-
-	return binary.NativeEndian.Uint16(b), nil
-}
-
-// Makes a cryptographically-secure random uint32.
-func RandomUint32() (uint32, error) {
-	b, err := RandomBytes(4)
-	if err != nil {
-		return 0, err
-	}
-
-	return binary.NativeEndian.Uint32(b), nil
-}
-
-// Makes a cryptographically-secure random uint64.
-func RandomUint64() (uint64, error) {
-	b, err := RandomBytes(8)
-	if err != nil {
-		return 0, nil
-	}
-
-	return binary.NativeEndian.Uint64(b), nil
+	return hex.EncodeToString(b), nil
 }
