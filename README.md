252d22ad18a745fc0f6ad3a15bb67f2a4853150c
Sion Pixley <sionpixley@gmail.com>
2024-11-10 19:15:05 -0600

    Change direction. Will now be a library.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# delirium
+# Delirium
+
+Delirium is a Go library that produces cryptographically-secure random values. Delirium is a wrapper around the standard library [crypto/rand](https://pkg.go.dev/crypto/rand), but it makes it easier to produce values of smaller size than crypto/rand is usually used for.
+
+## How to install
+
+`go get github.com/sionpixley/delirium`
+
+## How to use
+
+Make sure to include the import:
+
+`import "github.com/sionpixley/delirium/pkg/delirium"`
+
+### Producing random bytes
+
+`randomBytes := delirium.RandomBytes(<insert-num-of-bytes>)`
+
+### Producing random base64 strings
+
+`randomBase64 := delirium.RandomBase64String(<insert-length-of-string>)`
+
+### Producing random integers
+
+Delirium can produce random signed and unsigned integers of sizes: 16-bit, 32-bit, and 64-bit.
+
+```
+randomInt16 := delirium.RandomInt16()
+randomUint16 := delirium.RandomUint16()
+
+randomInt32 := delirium.RandomInt32()
+randomUint32 := delirium.RandomUint32()
+
+randomInt64 := delirium.RandomInt64()
+randomUint64 := delirium.RandomUint64()
+```
+
+### Producing random floats
+
+Delirium can produce random float32 or float64 values.
+
+```
+randomFloat32 := delirium.RandomFloat32()
+randomFloat64 := delirium.RandomFloat64()
+```
---
+++ pkg/delirium/delirium.go
@@ -0,0 +1,95 @@
+package delirium
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/binary"
+	"math"
+)
+
+func RandomBase64String(length int) (string, error) {
+	b, err := RandomBytes(length)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(b), nil
+}
+
+func RandomBytes(numOfBytes int) ([]byte, error) {
+	b := make([]byte, numOfBytes)
+	_, err := rand.Read(b)
+	return b, err
+}
+
+func RandomInt16() (int16, error) {
+	b, err := RandomBytes(2)
+	if err != nil {
+		return 0, nil
+	}
+
+	return int16(binary.NativeEndian.Uint16(b)), nil
+}
+
+func RandomInt32() (int32, error) {
+	b, err := RandomBytes(4)
+	if err != nil {
+		return 0, err
+	}
+
+	return int32(binary.NativeEndian.Uint32(b)), nil
+}
+
+func RandomInt64() (int64, error) {
+	b, err := RandomBytes(8)
+	if err != nil {
+		return 0, nil
+	}
+
+	return int64(binary.NativeEndian.Uint64(b)), nil
+}
+
+func RandomFloat32() (float32, error) {
+	b, err := RandomBytes(4)
+	if err != nil {
+		return 0, err
+	}
+
+	return math.Float32frombits(binary.NativeEndian.Uint32(b)), nil
+}
+
+func RandomFloat64() (float64, error) {
+	b, err := RandomBytes(8)
+	if err != nil {
+		return 0, nil
+	}
+
+	return math.Float64frombits(binary.NativeEndian.Uint64(b)), nil
+}
+
+func RandomUint16() (uint16, error) {
+	b, err := RandomBytes(2)
+	if err != nil {
+		return 0, nil
+	}
+
+	return binary.NativeEndian.Uint16(b), nil
+}
+
+func RandomUint32() (uint32, error) {
+	b, err := RandomBytes(4)
+	if err != nil {
+		return 0, err
+	}
+
+	return binary.NativeEndian.Uint32(b), nil
+}
+
+func RandomUint64() (uint64, error) {
+	b, err := RandomBytes(8)
+	if err != nil {
+		return 0, nil
+	}
+
+	return binary.NativeEndian.Uint64(b), nil
+}
